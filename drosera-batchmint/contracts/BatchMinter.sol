b70169d75cfb8c13976706dbc85d5d363393f8bc
Uzochurchill <155972439+Uzochurchill@users.noreply.github.com>
2025-09-24 17:19:45 +0100

    BatchMinter.sol

--- drosera-batchmint/contracts/BatchMinter.sol
+++ drosera-batchmint/contracts/BatchMinter.sol
@@ -1,21 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract BatchMintToken is ERC20, Ownable {
-    event Mint(address indexed user, uint256 amount);
+contract BatchMintNFT is ERC721, Ownable {
+    uint256 public nextTokenId;
+    event Mint(address indexed user, uint256 tokenId);
 
-    constructor() ERC20("Batch Mint Token", "BMT") {}
+    constructor() ERC721("Batch Mint NFT", "BMN") {}
 
     function batchMint(uint256 times) external {
         require(times > 0, "Times must be > 0");
 
-        // Example: 1 token per iteration (adjust decimals as needed)
-        uint256 totalAmount = times * 1e18;
-
-        _mint(msg.sender, totalAmount);
-        emit Mint(msg.sender, totalAmount);
+        for (uint256 i = 0; i < times; i++) {
+            uint256 tokenId = ++nextTokenId;
+            _safeMint(msg.sender, tokenId);
+            emit Mint(msg.sender, tokenId);
+        }
     }
 }
