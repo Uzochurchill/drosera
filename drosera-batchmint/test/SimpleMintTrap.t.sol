da3cbd31c511da56f62d9e447315cd91888e348b
Uzochurchill <155972439+Uzochurchill@users.noreply.github.com>
2025-09-25 13:05:38 +0100

    Update SimpleMintTrap.t.sol

--- drosera-batchmint/test/SimpleMintTrap.t.sol
+++ drosera-batchmint/test/SimpleMintTrap.t.sol
@@ -1,23 +1,60 @@
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
 import "forge-std/Test.sol";
 import "../contracts/SimpleMintTrap.sol";
-import "drosera-contracts/TrapResponse.sol";
+
+contract MockBatchMintNFT {
+    uint256 public nextTokenId;
+
+    function mint(uint256 amount) external {
+        nextTokenId += amount;
+    }
+}
 
 contract SimpleMintTrapTest is Test {
     SimpleMintTrap trap;
+    MockBatchMintNFT nft;
 
     function setUp() public {
-        trap = new SimpleMintTrap();
+        nft = new MockBatchMintNFT();
+        trap = new SimpleMintTrap(address(nft));
     }
 
-    function testTriggerAfterTooManyMints() public {
-        TrapResponse memory response;
-        for (uint i = 0; i < 6; i++) {
-            response = trap.handleLog(address(this), "");
-        }
-        assert(response.shouldRevert == true);
+    function testDetectsMintBurst() public {
+        // simulate previous block
+        nft.mint(2);
+        bytes memory prev = abi.encode(nft.nextTokenId());
+
+        // simulate current block with too many mints
+        nft.mint(10);
+        bytes memory latest = abi.encode(nft.nextTokenId());
+
+        bytes ;
+        samples[0] = prev;
+        samples[1] = latest;
+
+        (bool shouldRevert, bytes memory msgData) = trap.shouldRespond(samples);
+
+        assertTrue(shouldRevert, "Trap should trigger");
+        assertEq(abi.decode(msgData, (string)), "Too many mints in one block");
+    }
+
+    function testDoesNotTriggerForSmallMints() public {
+        // simulate previous block
+        nft.mint(1);
+        bytes memory prev = abi.encode(nft.nextTokenId());
+
+        // simulate current block with small mint
+        nft.mint(3);
+        bytes memory latest = abi.encode(nft.nextTokenId());
+
+        bytes ;
+        samples[0] = prev;
+        samples[1] = latest;
+
+        (bool shouldRevert, ) = trap.shouldRespond(samples);
+
+        assertFalse(shouldRevert, "Trap should not trigger");
     }
 }
